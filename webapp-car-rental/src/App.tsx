42d15fd8bf4b807f21a665e07a6a1cd91c8cbb7a
shanderb <s.hander8910@hotmail.com>
2025-04-18 17:46:42 -0300

    refactor: simplify App component structure by removing unnecessary elements and enhancing routing

--- webapp-car-rental/src/App.tsx
+++ webapp-car-rental/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home/Home';
+import BookingPage from './pages/Booking/Booking';
+import SuccessPage from './pages/Success/Success';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/booking" element={<BookingPage />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;

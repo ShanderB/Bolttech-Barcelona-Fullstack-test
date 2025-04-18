7cc9bf87dc112ec31a502d72d22e7551020300de
shanderb <s.hander8910@hotmail.com>
2025-04-18 20:25:36 -0300

    refactor: streamline car availability check by removing redundant logic and utilizing decrementCarStock function

--- backend-car-rental/src/controllers/booking-controller.ts
+++ backend-car-rental/src/controllers/booking-controller.ts
@@ -1,5 +1,5 @@
-import Car from '../models/car-model';
 import Booking from '../models/book-model';
+import { decrementCarStock } from './cars-controller';
 
 export const createBooking = async (req: any, res: any) => {
   const { carId, userId, startDate, endDate, licenseValid } = req.body;
@@ -31,14 +31,7 @@ export const createBooking = async (req: any, res: any) => {
     return res.status(400).json({ message: 'User already has a booking for the selected dates' });
   }
 
-  const car = await Car.findById(carId).select('_id stock');
-  if (!car) {
-    return res.status(404).json({ message: 'Car not found' });
-  }
 
-  if (!car?.stock) {
-    return res.status(400).json({ message: 'Car is not available' });
-  }
 
   // const session = await mongoose.startSession();
   // session.startTransaction();
@@ -65,8 +58,11 @@ export const createBooking = async (req: any, res: any) => {
   // });
 
   await booking.save();
-  car.stock! -= 1;
-  await car.save();
+  const decrementErrorMessage =  await decrementCarStock(carId, res);
+
+  if(decrementErrorMessage) {
+    return res.status(400).json({ message: decrementErrorMessage });
+  }
 
   //TODO validate correctly with then and catch
   //TODO add error test

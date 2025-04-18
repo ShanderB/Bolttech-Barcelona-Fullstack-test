ab0cdbb655e5226a55ff57f3859aef21a8ce0692
shanderb <s.hander8910@hotmail.com>
2025-04-18 20:26:46 -0300

    feat: add utility functions for calculating car prices and determining rental season

---
+++ backend-car-rental/src/shared/Utils.ts
@@ -0,0 +1,31 @@
+export const calculateCarPrices = (cars: any[], season: string, start: Date, end: Date) => {
+    const days = Math.ceil((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) + 1; // Calculate total days
+
+    return cars.map((car) => {
+        const dailyPrice = car.prices?.[season] ?? 0;
+        const totalPrice = parseFloat((dailyPrice * days).toFixed(2)); // Calculate total price for the rental period
+
+        return {
+            _id: car._id,
+            brand: car.brand,
+            model: car.model,
+            stock: car.stock! > 0 ? true : false,
+            price: dailyPrice,
+            totalPrice,
+        };
+    });
+};
+
+export const getSeason = (date: Date) => {
+    //could use moment.js or date-fns for better date handling
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate();
+
+    if ((month === 6 && day >= 1) || (month === 9 && day <= 15) || (month > 6 && month < 9)) {
+        return 'peak';
+    } else if ((month === 9 && day > 15) || (month === 10) || (month === 3) || (month === 5) || (month === 4)) {
+        return 'mid';
+    } else {
+        return 'off';
+    }
+};

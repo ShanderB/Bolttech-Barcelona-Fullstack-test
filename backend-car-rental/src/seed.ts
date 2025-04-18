a9962cced006fff9f9403d01f48336c50d37f6a6
shanderb <s.hander8910@hotmail.com>
2025-04-18 19:27:54 -0300

    fix: remove unnecessary comment in seedCars function

--- backend-car-rental/src/seed.ts
+++ backend-car-rental/src/seed.ts
@@ -36,6 +36,7 @@ const seedCars = async () => {
   ];
 
   try {
+    //todo remove string
     await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/car-rental');
     console.log('Connected to MongoDB');
 

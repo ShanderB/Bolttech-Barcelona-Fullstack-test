1c283c6d1229278edf1ce35c4222a121e207bd47
shanderb <s.hander8910@hotmail.com>
2025-04-18 21:03:07 -0300

    refactor: enhance SuccessPage styling for dark mode support

--- webapp-car-rental/src/pages/Success/Success.tsx
+++ webapp-car-rental/src/pages/Success/Success.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import './success.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useTheme } from '../../shared/ThemeContext';
 
 const SuccessPage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const car = location.state?.car;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { isDarkMode } = useTheme();
+    const car = location.state?.car;
 
-  return (
-    <div className="success-container">
-      <h1>Booking Confirmed!</h1>
-      {car ? (
-        <div className="car-details">
-          <p><strong>Brand:</strong> {car.brand}</p>
-          <p><strong>Model:</strong> {car.model}</p>
-          <p><strong>Price per day:</strong> ${car.price}</p>
-          <p><strong>Total Price:</strong> ${car.totalPrice}</p>
+    return (
+        <div className={`success-container ${isDarkMode ? 'dark' : ''}`}>
+            <h1>Booking Confirmed!</h1>
+            {car ? (
+                <div className="car-details">
+                    <p><strong>Brand:</strong> {car.brand}</p>
+                    <p><strong>Model:</strong> {car.model}</p>
+                    <p><strong>Price per day:</strong> ${car.price}</p>
+                    <p><strong>Total Price:</strong> ${car.totalPrice}</p>
+                </div>
+            ) : (
+                <p>No car details available.</p>
+            )}
+            <button className="home-button" onClick={() => navigate('/')}>
+                Go Back to Home
+            </button>
         </div>
-      ) : (
-        <p>No car details available.</p>
-      )}
-      <button className="home-button" onClick={() => navigate('/')}>
-        Go Back to Home
-      </button>
-    </div>
-  );
+    );
 };
 
 export default SuccessPage;

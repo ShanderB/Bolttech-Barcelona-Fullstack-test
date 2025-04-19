492bbafd649db4b17340f5a5bd2ce8d8eb918d13
shanderb <s.hander8910@hotmail.com>
2025-04-18 21:04:08 -0300

    feat: wrap Router component with ThemeProvider for improved theming support

--- webapp-car-rental/src/App.tsx
+++ webapp-car-rental/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/Home';
-import BookingPage from './pages/Booking/Booking';
 import SuccessPage from './pages/Success/Success';
+import { ThemeProvider } from './shared/ThemeContext';
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/success" element={<SuccessPage />} />
-    </Routes>
-  </Router>
+  <ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/success" element={<SuccessPage />} />
+      </Routes>
+    </Router>
+  </ThemeProvider>
+
 );
 
 export default App;
